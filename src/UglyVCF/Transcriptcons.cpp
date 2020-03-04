90634a47bc35df31ed80b4eb2eee333ffb7d40ff
carmen32 <57679272+carmen32@users.noreply.github.com>
2020-03-04 15:12:47 +0100

    smallchanges

--- src/UglyVCF/Transcriptcons.cpp
+++ src/UglyVCF/Transcriptcons.cpp
@@ -195,7 +195,7 @@ QString Transcriptcons::printtranscons(Transcriptcons trans){
         li = li +","+qli.at(i);  }
 
     re = re + trans.getTranscript_id() +",\n IMPACT: " + trans.getImpact()+",\n VARIANT_ALLELE: " + trans.getVariant_allele()
-       +",\n GENE_SYMBOLE" + trans.getGene_symbole()+",\n GENE_SYMBOLE_SOURCE: "+ trans.getGene_symbol_source()
+       +",\n GENE_SYMBOLE: " + trans.getGene_symbole()+",\n GENE_SYMBOLE_SOURCE: "+ trans.getGene_symbol_source()
        +",\n GENE_ID: "+ trans.getGene_id()+",\n HGNC: " + trans.getHgnc_id()+",\n STRAND: "
        + trans.getStrand()+",\n BIOTYPE: " + trans.getBiotype()+",\n DISTANCE: " + trans.getDistance()+",[" + li + "] \n";
     return re;
