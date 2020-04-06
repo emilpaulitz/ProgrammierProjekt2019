f82113b04d5aa03ec226947070c8edfe6a69d677
HomoPolyethylen <casimir.grote@gmail.com>
2020-04-06 23:48:26 +0200

    adjusted echo logs for status bar usage

--- src/Pipeline/pipeline.sh
+++ src/Pipeline/pipeline.sh
@@ -1,89 +1,81 @@
 #!/bin/bash
-# usage: ./pipeline.sh [read1] [read2] [ref]
+# usage: ./pipeline.sh [read1] [read2] [ref] [WD]
 # ./pipeline.sh ~/Desktop/Data/NA12878_03_AACGTGAT_L001_R1_001.fastq.gz ~/Desktop/Data/NA12878_03_AACGTGAT_L001_R2_001.fastq.gz ~/Desktop/Data/GRCh37.fa
-
 
 # clear old cache
 #./empty-cache.sh
 # make alias for colourful echoing progress
-#alias cecho='f(){ echo -e "\e[33m$@  \e[0m";  unset -f f; }; f'
+#alias cecho='f(){ echo -e "\e[33m$@ ";  unset -f f; }; f'
 
 # copy data to cache
-echo -e "\e[33m copying data... \e[0m"
+echo "copying data..."
 cp $1 cache/read1.fastq.gz
 cp $2 cache/read2.fastq.gz
 cp $3* cache
-echo -e "\e[33m done \e[0m"
+echo "done"
 
-echo -e "\e[33m adding references... \e[0m"
+echo "adding references..."
 READ1=$PWD/cache/read1.fastq.gz
 READ2=$PWD/cache/read2.fastq.gz
 REF=$PWD/cache/GRCh37.fa
-echo -e "\e[33m done \e[0m"
 
 ## [.fastQ]
 #adapter trimming <SeqPurge>
-echo -e "\e[33m adapter trimming... \e[0m"
+echo "adapter trimming..."
 ~/miniconda3/bin/SeqPurge -in1 $READ1 -in2 $READ2 -out1 cache/R1_purged.fastq.gz -out2 cache/R2_purged.fastq.gz
 READ1=$PWD/cache/R1_purged.fastq.gz
 READ2=$PWD/cache/R2_purged.fastq.gz
-echo -e "\e[33m done \e[0m"
 
 
 #mapping <bwa>
-echo -e "\e[33m mapping... \e[0m"
+echo "mapping..."
 cd bwa
 ./bwa index $REF
 ./bwa mem $REF $READ1 $READ2 > ../cache/aligned.sam
 cd ..
-echo -e "\e[33m done \e[0m"
 ## [.sam]
 
 #duplicate removal <samblaster>
+echo "duplicate removal..."
 samblaster/samblaster -M --removeDups -i cache/aligned.sam > cache/dupsRemoved.sam
 
 #convert sam to bam <samtools> sam2bam sort index
-echo -e "\e[33m converting sam->bam... \e[0m"
+echo "converting sam->bam..."
 cd cache
 samtools view -ub dupsRemoved.sam -o converted.bam
 samtools sort converted.bam -o sorted.bam
 samtools index sorted.bam
 BAM=$PWD/sorted.bam
 cd ..
-echo -e "\e[33m done \e[0m"
 ## [.bam]
 
 #indel realignment <ABRA2>
-echo -e "\e[33m indel realignment... \e[0m"
+echo "indel realignment..."
 TARGET=$PWD/../../data/target_region.bed
 java -Xmx4G -jar abra2/temp/abra-0.94b.jar --ref $REF --in $BAM --out cache/indel.bam --working cache/tmpAbra --targets $TARGET
 BAM=$PWD/cache/indel.bam
-echo -e "\e[33m abra cadabra done \e[0m"
+echo "abra cadabra done"
 
 #variant calling <freebayes>
 ## [.vcf]
-echo -e "\e[33m variant calling... \e[0m"
+echo "variant calling..."
 freebayes -f $REF $BAM > cache/free.vcf
-echo -e "\e[33m done \e[0m"
 
 # variant normalization <vcfallelicprimitives>
 # usage: vcfallelicprimitives [options][file]
-echo -e "\e[33m variant normalization... \e[0m"
+echo "variant normalization..."
 ~/miniconda3/pkgs/vcflib-1.0.0_rc3-py37hc088bd4_0/bin/vcfallelicprimitives -k cache/free.vcf > cache/norm.vcf
-echo -e "\e[33m done \e[0m"
 
 # break multi allelic variants <vcfbreakmulti>
 # usage: vcfbreakmulti [options][file]
-echo -e "\e[33m break multi allelic variants... \e[0m"
+echo "break multi allelic variants..."
 ./ngs-bits-master/bin/VcfBreakMulti -in cache/norm.vcf -out cache/break.vcf
-echo -e "\e[33m done \e[0m"
 
 # left-align variants <VcfLeftNormalize>
-echo -e "\e[33m left-align variants... \e[0m"
+echo "left-align variants..."
 ./ngs-bits-master/bin/VcfLeftNormalize -in cache/break.vcf -out cache/leftvar.vcf -ref $REF
-echo -e "\e[33m done \e[0m"
 
 # sort <VcfSort>
-echo -e "\e[33m sort vcf file... \e[0m"
+echo "sort vcf file..."
 ./ngs-bits-master/bin/VcfSort -in cache/leftvar.vcf -out cache/final.vcf
-echo -e "\e[33m done \e[0m"
+echo "done"
